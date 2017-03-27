167029e41a17b9a506b7c578208d94917b16e6c1
BjornDCode <bjorn@lindholmhansen.com>
2017-03-27 19:55:00 +0200

    Mandag - skole

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var body = document.querySelector('body');
 
 
 // Use slider on mobile
-(function() {
+var slider = (function() {
     var sliderSection = body.querySelector('.slider-section');
     var output = sliderSection.querySelector('.output');
     var slider = sliderSection.querySelector('.slider');
@@ -13,6 +13,79 @@ var body = document.querySelector('body');
         var days = e.target.value;
         output.innerHTML = days + ' days';
         body.className = 'days' + days;
+        impact.updateNumbers(Number(days));
+        savings.updateNumbers(Number(days));
     }
 
+})();
+
+// Update Impact Section
+var impact = (function() {
+    var impactSection = body.querySelector('.impact-section');
+
+    var meatOutputContainer = impactSection.querySelector('.meat-output');
+    var waterOutputContainer = impactSection.querySelector('.water-output');
+    var landOutputContainer = impactSection.querySelector('.land-output');
+    var treesOutputContainer = impactSection.querySelector('.trees-output');
+    var co2OutputContainer = impactSection.querySelector('.co2-output');
+    var wasteOutputContainer = impactSection.querySelector('.waste-output');
+
+    var dataPerDay = {
+        meat: 48, // KG of meat per person per year
+        water: 8000, // Liters of water per KG of meat (average of chicken, pork and beef)
+        land: 1, // km2 pr kg of meat per year
+        trees: 250.000, // Number of average trees pr. km2
+        co2: 68.75, // kg of co2 per kg of meat
+        waste: 3 // Not done
+    };
+
+    function updateNumbers(days) {
+        meatOutputContainer.innerHTML = Math.round(dataPerDay['meat'] / 7 * days);
+        waterOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['water'] / 7 * days);
+        landOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['land'] / 7 * days);
+        treesOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['land'] * dataPerDay['trees'] / 7 * days);
+        co2OutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['co2'] / 7 * days);
+        wasteOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['waste'] / 7 * days);
+    }
+
+    updateNumbers(5);
+
+    return {
+        updateNumbers: updateNumbers
+    }
+})();
+
+var savings = (function() {
+    var savingsSection = body.querySelector('.savings-section');
+
+    var meatOutputContainer = savingsSection.querySelector('.meat-output');
+    var waterOutputContainer = savingsSection.querySelector('.water-output');
+    var landOutputContainer = savingsSection.querySelector('.land-output');
+    var treesOutputContainer = savingsSection.querySelector('.trees-output');
+    var co2OutputContainer = savingsSection.querySelector('.co2-output');
+    var wasteOutputContainer = savingsSection.querySelector('.waste-output');
+
+    var dataPerDay = {
+        meat: 48, // KG of meat per person per year
+        water: 8000, // Liters of water per KG of meat (average of chicken, pork and beef)
+        land: 1, // km2 pr kg of meat per year
+        trees: 250.000, // Number of average trees pr. km2
+        co2: 68.75, // kg of co2 per kg of meat
+        waste: 3 // Not done
+    };
+
+    function updateNumbers(days) {
+        meatOutputContainer.innerHTML = Math.round(dataPerDay['meat'] / 7);
+        waterOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['water'] / 7);
+        landOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['land'] / 7);
+        treesOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['land'] * dataPerDay['trees'] / 7);
+        co2OutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['co2'] / 7);
+        wasteOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['waste'] / 7);
+    }
+
+    updateNumbers(5);
+
+    return {
+        updateNumbers: updateNumbers
+    }
 })();
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>CAMPAIGN NAME</title>
+        <title>Meat Matters for Greenpeace</title>
         <link rel="stylesheet" href="assets/css/normalize.css">
         <link rel="stylesheet" href="assets/css/style.css">
     </head>
@@ -14,6 +14,88 @@
                 <h3>How many times per week do you eat meat?</h3>
                 <h2 class="output">5 days</h2>
                 <input type="range" name="daySlider" min="1" max="7" value="5" step="1" class="slider">
+            </div>
+        </section>
+
+        <section class="main-section impact-section">
+            <div class="headline">
+                <h3>Your impact</h3>
+                <span>pr. year</span>
+            </div>
+            <div class="table">
+                <div class="factor">
+                    <img src="assets/images/cow.svg" alt="Meat">
+                    <p><span class="meat-output">21</span> kg. of Meat</p>
+                </div>
+                <div class="factor">
+                    <img src="assets/images/water.png" alt="Water">
+                    <p><span class="water-output">120.000</span> Liters of Water</p>
+                </div>
+                <div class="factor">
+                    <img src="assets/images/land.svg" alt="Land">
+                    <p><span class="land-output">3.000</span> km2 of Land</p>
+                </div>
+                <div class="factor">
+                    <img src="assets/images/trees.png" alt="Trees">
+                    <p><span class="trees-output">45</span> Trees</p>
+                </div>
+                <div class="factor">
+                    <img src="assets/images/co2.png" alt="CO2">
+                    <p><span class="co2-output">1.2</span> Tons of CO2</p>
+                </div>
+                <div class="factor">
+                    <img src="assets/images/waste.png" alt="Waste">
+                    <p><span class="waste-output">4</span> Tons of Waste</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="main-section savings-section">
+            <div class="headline">
+                <h3>Your savings</h3>
+                <span>if you eat meat 1 day less per week</span>
+            </div>
+            <div class="table">
+                <div class="factor">
+                    <img src="assets/images/cow.svg" alt="Meat">
+                    <p><span class="meat-output">21</span> kg. of Meat</p>
+                </div>
+                <div class="factor">
+                    <img src="assets/images/water.png" alt="Water">
+                    <p><span class="water-output">120.000</span> Liters of Water</p>
+                </div>
+                <div class="factor">
+                    <img src="assets/images/land.svg" alt="Land">
+                    <p><span class="land-output">3.000</span> km2 of Land</p>
+                </div>
+                <div class="factor">
+                    <img src="assets/images/trees.png" alt="Trees">
+                    <p><span class="trees-output">45</span> Trees</p>
+                </div>
+                <div class="factor">
+                    <img src="assets/images/co2.png" alt="CO2">
+                    <p><span class="co2-output">1.2</span> Tons of CO2</p>
+                </div>
+                <div class="factor">
+                    <img src="assets/images/waste.png" alt="Waste">
+                    <p><span class="waste-output">4</span> Tons of Waste</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="main-section information-section">
+            <h3>More information</h3>
+            <div class="info-container">
+                <div class="text">
+                    <p>Phasellus tristique massa sed ligula euismod suscipit. Phasellus gravida congue est, nec suscipit ipsum posuere in. Morbi in purus sapien. Nullam ornare tincidunt ligula, et condimentum ex suscipit quis. Morbi pulvinar mattis lacus id dictum. Vestibulum et lectus arcu. In viverra, metus egestas commodo tincidunt, ex odio semper ipsum, cursus congue risus lorem ut magna.</p>
+                    <p>Donec maximus odio magna, vel maximus ante elementum vitae. Donec sit amet urna arcu. Praesent varius lacus nec mattis feugiat. Ut consequat, quam sit amet porttitor fringilla, arcu mauris mollis orci, nec laoreet justo nisl et felis. Suspendisse consequat auctor volutpat. Ut iaculis elit nibh, a finibus dolor interdum quis. Curabitur leo turpis, tincidunt a ornare vitae, pulvinar vel mauris. Pellentesque sollicitudin vulputate risus, sed iaculis mauris pretium ut.</p>
+                    <p>Vivamus mollis feugiat dignissim. Etiam nec ullamcorper magna. Phasellus dolor ligula, efficitur et dapibus vel, pharetra facilisis neque. Sed hendrerit ut erat at egestas. Aliquam lectus felis, sodales nec nibh quis, ornare vestibulum purus. Cras viverra augue urna, non porttitor libero rutrum eu. Donec placerat metus a ullamcorper facilisis.</p>
+                </div>
+                <div class="more-info-sections">
+                    <a href="#" class="more-info-link">More information about greenhouse emissions</a>
+                    <a href="#" class="more-info-link">How you can reduce your meat consumption</a>
+                    <a href="#" class="more-info-link">Cras viverra augue urna non porttitor libero</a>
+                </div>
             </div>
         </section>
 
