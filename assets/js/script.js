f000d9a9ef2bdd4f46fa1c074e46aace95a8883f
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-03-28 19:14:12 +0200

    Calculate button & Share buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,49 @@
 var body = document.querySelector('body');
 
 
+var calculateButton = (function() {
+    var calculateButton = body.querySelector('.calculate');
+    var impactSection = $('.impact-section');
+    calculateButton.addEventListener('click', function() {
+        impact.updateNumbers(slider.getDays());
+        savings.updateNumbers(slider.getDays());
+        $('html, body').animate({
+            scrollTop: impactSection.offset().top
+        }, 300);
+        // window.pageYOffset = impactSection.offsetTop;
+    });
+})();
+
 // Use slider on mobile
 var slider = (function() {
     var sliderSection = body.querySelector('.slider-section');
     var output = sliderSection.querySelector('.output');
     var slider = sliderSection.querySelector('.slider');
-    var impactSection = body.querySelector('.impact-section');
-    var savingsSection = body.querySelector('.savings-section');
+    // var impactSection = body.querySelector('.impact-section');
+    // var savingsSection = body.querySelector('.savings-section');
     slider.addEventListener('change', updateDays);
+    var days = 5;
 
     function updateDays(e) {
-        var days = e.target.value;
+        days = Number(e.target.value);
         output.innerHTML = days + ' days';
         body.className = 'days' + days;
 
-        window.addEventListener('scroll', function() {
-            if (window.pageYOffset > impactSection.offsetTop / 2 || window.pageYOffset > savingsSection.offsetTop) {
-                impact.meatOutputContainer.classList.remove('hasRun');
-                impact.updateNumbers(Number(days));
-            }
-        });
-        savings.updateNumbers(Number(days));
+        // window.addEventListener('scroll', function() {
+        //     if (window.pageYOffset > impactSection.offsetTop / 2 || window.pageYOffset > savingsSection.offsetTop) {
+        //         impact.meatOutputContainer.classList.remove('hasRun');
+        //         impact.updateNumbers(Number(days));
+        //     }
+        // });
     }
+
+    function getDays() {
+        return days;
+    }
+
+    return {
+        getDays: getDays
+    };
 
 })();
 
@@ -37,7 +58,7 @@ var countUp = (function() {
       suffix : ''
     };
     function countElement(id, limit) {
-        var elem = new CountUp(id, 0, limit, 0, 2.5, options);
+        var elem = new CountUp(id, 0, limit, 0, 3.5, options);
         return elem;
     }
     return {
@@ -67,20 +88,26 @@ var impact = (function() {
 
     function updateNumbers(days) {
         // meatOutputContainer.innerHTML = Math.round(dataPerDay['meat'] / 7 * days).toLocaleString();
-        if (!meatOutputContainer.classList.contains('hasRun')) {
-            meatOutputContainer.innerHTML = countUp.countElement(meatOutputContainer ,Math.round(dataPerDay['meat'] / 7 * days)).start();
-            meatOutputContainer.classList.add('hasRun');
-        }
+        // if (!meatOutputContainer.classList.contains('hasRun')) {
+        //     meatOutputContainer.innerHTML = countUp.countElement(meatOutputContainer ,Math.round(dataPerDay['meat'] / 7 * days)).start();
+        //     meatOutputContainer.classList.add('hasRun');
+        // }
 
-        waterOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['water'] / 7 * days).toLocaleString();
-        landOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['land'] / 7 * days).toLocaleString();
-        treesOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['land'] * dataPerDay['trees'] / 7 * days).toLocaleString();
-        co2OutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['co2'] / 7 * days).toLocaleString();
-        wasteOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['waste'] / 7 * days).toLocaleString();
+        meatOutputContainer.innerHTML = countUp.countElement(meatOutputContainer, Math.round(dataPerDay['meat'] / 7 * days)).start();
+        waterOutputContainer.innerHTML = countUp.countElement(waterOutputContainer, Math.round(dataPerDay['meat'] * dataPerDay['water'] / 7 * days)).start();
+        landOutputContainer.innerHTML = countUp.countElement(landOutputContainer, Math.round(dataPerDay['meat'] * dataPerDay['land'] / 7 * days)).start();
+        treesOutputContainer.innerHTML = countUp.countElement(treesOutputContainer, Math.round(dataPerDay['meat'] * dataPerDay['land'] * dataPerDay['trees'] / 7 * days)).start();
+        co2OutputContainer.innerHTML = countUp.countElement(co2OutputContainer, Math.round(dataPerDay['meat'] * dataPerDay['co2'] / 7 * days)).start();
+        wasteOutputContainer.innerHTML = countUp.countElement(wasteOutputContainer, Math.round(dataPerDay['meat'] * dataPerDay['waste'] / 7 * days)).start();
+        // waterOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['water'] / 7 * days).toLocaleString();
+        // landOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['land'] / 7 * days).toLocaleString();
+        // treesOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['land'] * dataPerDay['trees'] / 7 * days).toLocaleString();
+        // co2OutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['co2'] / 7 * days).toLocaleString();
+        // wasteOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['waste'] / 7 * days).toLocaleString();
     }
 
 
-    updateNumbers(5);
+    // updateNumbers(5);
 
     return {
         updateNumbers: updateNumbers,
@@ -116,7 +143,7 @@ var savings = (function() {
         wasteOutputContainer.innerHTML = Math.round(dataPerDay['meat'] * dataPerDay['waste'] / 7).toLocaleString();
     }
 
-    updateNumbers(5);
+    // updateNumbers(5);
 
     return {
         updateNumbers: updateNumbers
@@ -129,3 +156,15 @@ $(document).alton({
     useSlideNumbers: false, // Set to false if you don't want to use pagination
     bodyContainer: 'body', // Tell Alton the body class
 });
+
+// Icons Transition
+// var iconTransition = (function() {
+//     var tables = body.querySelectorAll('.table');
+//     window.addEventListener('scroll', function() {
+//         for (var i = 0; i < tables.length; i++) {
+//             if (window.pageYOffset > tables[i].offsetTop / 2) {
+//                 tables[i].classList.add('visible');
+//             }
+//         }
+//     });
+// })();
--- index.php
+++ index.php
@@ -7,6 +7,7 @@
                 <h3>How many times per week do you eat meat?</h3>
                 <h2 class="output">5 days</h2>
                 <input type="range" name="daySlider" min="1" max="7" value="5" step="1" class="slider">
+                <button type="button" name="calculate" class="calculate">Calculate</button>
             </div>
         </section>
 
@@ -15,30 +16,34 @@
                 <h3>Your impact</h3>
                 <span>pr. year</span>
             </div>
+            <div class="social">
+                <a href="#0" class="facebook"></a>
+                <a href="#0" class="twitter"></a>
+            </div>
             <div class="table">
                 <div class="factor">
                     <img src="assets/images/cow.svg" alt="Meat">
-                    <p><span class="meat-output">21</span> kg. of Meat</p>
+                    <p><span class="meat-output">0</span> kg. of Meat</p>
                 </div>
                 <div class="factor">
                     <img src="assets/images/water.png" alt="Water">
-                    <p><span class="water-output">120.000</span> Liters of Water</p>
+                    <p><span class="water-output">0</span> Liters of Water</p>
                 </div>
                 <div class="factor">
                     <img src="assets/images/land.svg" alt="Land">
-                    <p><span class="land-output">3.000</span> km2 of Land</p>
+                    <p><span class="land-output">0</span> km2 of Land</p>
                 </div>
                 <div class="factor">
                     <img src="assets/images/trees.png" alt="Trees">
-                    <p><span class="trees-output">45</span> Trees</p>
+                    <p><span class="trees-output">0</span> Trees</p>
                 </div>
                 <div class="factor">
                     <img src="assets/images/co2.png" alt="CO2">
-                    <p><span class="co2-output">1.2</span> Tons of CO2</p>
+                    <p><span class="co2-output">0</span> Tons of CO2</p>
                 </div>
                 <div class="factor">
                     <img src="assets/images/waste.png" alt="Waste">
-                    <p><span class="waste-output">4</span> Tons of Waste</p>
+                    <p><span class="waste-output">0</span> Tons of Waste</p>
                 </div>
             </div>
         </section>
@@ -48,30 +53,34 @@
                 <h3>Your savings</h3>
                 <span>if you eat meat 1 day less per week</span>
             </div>
+            <div class="social">
+                <a href="#0" class="facebook"></a>
+                <a href="#0" class="twitter"></a>
+            </div>
             <div class="table">
                 <div class="factor">
                     <img src="assets/images/cow.svg" alt="Meat">
-                    <p><span class="meat-output">21</span> kg. of Meat</p>
+                    <p><span class="meat-output">0</span> kg. of Meat</p>
                 </div>
                 <div class="factor">
                     <img src="assets/images/water.png" alt="Water">
-                    <p><span class="water-output">120.000</span> Liters of Water</p>
+                    <p><span class="water-output">0</span> Liters of Water</p>
                 </div>
                 <div class="factor">
                     <img src="assets/images/land.svg" alt="Land">
-                    <p><span class="land-output">3.000</span> km2 of Land</p>
+                    <p><span class="land-output">0</span> km2 of Land</p>
                 </div>
                 <div class="factor">
                     <img src="assets/images/trees.png" alt="Trees">
-                    <p><span class="trees-output">45</span> Trees</p>
+                    <p><span class="trees-output">0</span> Trees</p>
                 </div>
                 <div class="factor">
                     <img src="assets/images/co2.png" alt="CO2">
-                    <p><span class="co2-output">1.2</span> Tons of CO2</p>
+                    <p><span class="co2-output">0</span> Tons of CO2</p>
                 </div>
                 <div class="factor">
                     <img src="assets/images/waste.png" alt="Waste">
-                    <p><span class="waste-output">4</span> Tons of Waste</p>
+                    <p><span class="waste-output">0</span> Tons of Waste</p>
                 </div>
             </div>
         </section>
